468465cb46e68ab84486d83cd610538f5a50a93e
stevenmjl <stevenmjl.24@gmail.com>
2025-02-03 18:54:20 -0400

    Creando el Service de Modelo.

---
+++ Services/ModelosService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Steven_Javier_P1_AP1.Modelos;
+using RegistroTecnicos.DAL;
+using System.Linq.Expressions;
+
+namespace Steven_Javier_P1_AP1.Services
+{
+    public class ModelosService(IDbContextFactory<Contexto> DbFactory)
+    {
+        // Existe
+        // Insertar
+        // Modificar
+        // Guardar
+        // Buscar
+        // Eliminar
+        // Listar
+
+    }
+}
