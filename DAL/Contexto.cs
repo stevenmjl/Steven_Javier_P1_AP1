e4918589113e9a27a89cc462acfb44c219a4bf94
stevenmjl <stevenmjl.24@gmail.com>
2025-02-03 18:47:55 -0400

    Creando el contexto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Steven_Javier_P1_AP1.Modelos;
+
+namespace RegistroTecnicos.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Modelo> Modelo { get; set; }
+}
